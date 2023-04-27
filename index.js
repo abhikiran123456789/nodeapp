fb6608a54027f74186b1b721a905321c765f96d1
Ubuntu <ubuntu@ip-172-31-20-168.ec2.internal>
2023-04-27 12:23:50 +0000

    nodegs

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var express = require(‘express’);
 var app = express();
 app.get(‘/’, function (req, res) {
- res.send(‘hello world’);
+ res.send(‘hello abhi’);
 });//listen to port 80 by default
 app.listen(process.env.PORT || 80);
 module.exports = app;
